8d0f7c917d1b735bcb622a9b77b1face9fa359af
henriquesml <henrique_schmeller@hotmail.com>
2019-12-12 21:11:47 -0300

    cirando upload de imagem e edição de um User

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,16 @@ import bulmaQuickview from 'bulma-quickview/src/js'
  
 document.addEventListener('turbolinks:load', function() {
   let quickviews = bulmaQuickview.attach()
+
+  const fileInput = document.querySelectorAll('input[type=file].file-input')
+  fileInput.forEach(input => {
+    input.onchange = () => {
+      if (input.files.length > 0) {
+        const fileName = input.parentNode.querySelector(".file-name")
+        fileName.textContent = input.files[0].name
+      }
+    }
+  })
 })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
